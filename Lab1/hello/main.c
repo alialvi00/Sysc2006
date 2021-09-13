297d4d7c9a34faeaf31e208762ddf91d0af44151
alialvi00 <ali_mashhood98@hotmail.com>
2021-09-13 14:57:39 -0400

    Lab1 completed

---
+++ Lab1/hello/main.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* main: generate some simple output. */
+
+int main(void)
+{
+    printf("Hello, world!\n");
+    printf("C programming is fun! \n");
+    return EXIT_SUCCESS;
+}
