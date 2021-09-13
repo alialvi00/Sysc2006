35920b5348dbbca7ac3966655cd6ec5da8ec972b
alialvi00 <ali_mashhood98@hotmail.com>
2021-09-13 14:44:09 -0400

    Renamed folders

---
+++ Lab1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  lab0
+//
+//  Created by Ali Alvi on 2021-09-13.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
