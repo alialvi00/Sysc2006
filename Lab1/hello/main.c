f6dc850ebfd72ff58d7c59572b352c37565398f8
alialvi00 <ali_mashhood98@hotmail.com>
2021-09-23 22:15:15 -0400

    Completed lab1 for sysc2006

--- Lab1/hello/main.c
+++ Lab1/hello/main.c
@@ -1,8 +1,21 @@
+//Ali Alvi
+//Student Number: 101114940
+
 
 #include <stdio.h>
 #include <stdlib.h>
 
 /* main: generate some simple output. */
+
+
+//a) Error Message: Build failed, "Expected '}'
+//b) Warning Message: Type specifier missing, defaults to 'int'
+//c) Error Message: Undefined symbol: _main
+//d) Error Message: Unterminated /* comment
+//e) Implicit declaration of function 'pintf' is invalid in C99
+//f) Error Message: Expected ')'
+//g) Error Message: Extraneous ')' before ';'
+//h) Error Message: Expected ';' after return statement
 
 int main(void)
 {
